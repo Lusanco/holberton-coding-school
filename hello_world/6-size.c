69e90c094e5d65954ec0c03a782d9a9fa2a55adc
root <root@debianLaptop.debianLaptop>
2023-10-12 13:19:20 -0400

    First test commit

---
+++ hello_world/6-size.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints size of
+ * various types on the
+ * computer it is compiled
+ * and run on.
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	/*Variables*/
+	int myInt;
+	float myFloat;
+	long myLong;
+	long long int myLolong;
+	char myChar;
+
+	printf(myChar);
+	printf(myInt);
+	printf(myLong);
+	printf(myLolong);
+	printf(myFloat);
+
+	return (0);
+}
