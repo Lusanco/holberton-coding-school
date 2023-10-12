bd385cf97e874901729c1a97f8c00cd8fa899d85
root <root@debianLaptop.debianLaptop>
2023-10-12 14:26:12 -0400

    Write a C program that prints, followed by a new line, to the standard error.

---
+++ hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print followed by 
+ * new line, to the standard 
+ * error.
+ * Return: 1
+ */
+
+int main(void)
+
+{
+	printf("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	stderr;
+	return (1);
+}
