072d0b8a09e190946dfeaeeba83b9a863322ff65
root <root@debianLaptop.debianLaptop>
2023-10-12 15:12:46 -0400

    Fix including library

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
- * main - Print followed by 
- * new line, to the standard 
+ * main - Print followed by
+ * new line, to the standard
  * error.
  * Return: 1
  */
@@ -10,7 +12,8 @@
 int main(void)
 
 {
-	printf("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-	stderr;
+	char msg[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, msg, strlen(msg));
 	return (1);
 }
