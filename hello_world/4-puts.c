3fd9e7e0454de3fff61234e46fe500096fd16162
root <root@debianLaptop.debianLaptop>
2023-10-12 10:24:18 -0400

    Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints text using 'puts'
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
