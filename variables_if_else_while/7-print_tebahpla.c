e2c4f3567ec94cf63f2e11b2308a4befb53bbf4f
root <root@debianLaptop.debianLaptop>
2023-10-16 13:48:35 -0400

    Write a program that prints the lowercase alphabet in reverse, followed by a new line.

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in reverse
+ * 
+ * Return:0
+ */
+
+
+int main(void)
+{
+	char abz;
+
+	for (abz = 'z'; abz >= 'a'; abz--)
+	{
+		putchar(abz);
+	}
+	putchar('\n');
+	return (0);
+}
