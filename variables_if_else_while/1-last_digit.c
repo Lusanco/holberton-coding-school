0d0650ae701ee82e225b051f2c3db9308da7aba2
root <root@debianLaptop.debianLaptop>
2023-10-16 10:18:09 -0400

    Fixed Wrong Variable

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	lastN = n % 10;
 	/* your code goes there */
-	if (n > 5)
+	if (lastN > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastN);
 	}
-	else if (n == 0)
+	else if (lastN == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastN);
 	}
-	else if (n < 6 && n != 0)
+	else if (lastN < 6 && lastN != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, lastN);
 	}
