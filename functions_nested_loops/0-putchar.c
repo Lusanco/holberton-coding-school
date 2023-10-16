fa26181f2faf98f6c5c696d56c405154c645edfa
root <root@debianLaptop.debianLaptop>
2023-10-16 17:12:06 -0400

    Fix 2

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,14 +6,6 @@
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
+	printf("_putchar\n");
+	return 0;
 }
