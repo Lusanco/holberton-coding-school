02aa8b81126b25bcc339965f1dcc1a6d296c0d50
root <root@debianLaptop.debianLaptop>
2023-10-17 14:14:19 -0400

    Write a function that prints the last digit of a number.

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ *
+ * @n: takes an number
+ *
+ * Return: last
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+
+	last = n % 10;
+
+	_putchar('0' + last);
+	return (last);
+}
