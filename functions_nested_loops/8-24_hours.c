dd18e02bb9315b353d85f36a58deea98bf5e1ce8
Lusanco <93743011+Lusanco@users.noreply.github.com>
2023-10-17 18:35:22 -0400

    Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#iinclude "main.h"
+
+/**
+ * jack_bauer - print every minute
+ */
+
+void jack_bauer(void)
+{
+	int hr;
+	int min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + hr / 10);
+			_putchar('0' + hr % 10);
+			_putchar(':');
+			_putchar('0' + min / 10);
+			_putchar('0' + min % 10);
+			_putchar('\n');
+		}
+	}
+}
