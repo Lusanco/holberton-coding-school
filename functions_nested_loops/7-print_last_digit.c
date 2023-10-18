f2a4ffec6558499f91ad0f6a2bd7125caa079cf8
root <root@debianLaptop.debianLaptop>
2023-10-18 14:46:12 -0400

    Fix code

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,31 +11,16 @@
 
 int print_last_digit(int n)
 {
-	int last;
+	n = n % 10;
 
 	if (n < 0)
 	{
-		last = -n % 10;
-		_putchar('0' + last);
-		return last;
+		n = -n;
+		_putchar('0' + n);
 	}
 	else
 	{
-		last = n % 10;
-		_putchar('0' + last);
-		return last;
+		_putchar('0' + n);
 	}
+	return (n);
 }
-/**{
-	int last;
-
-	if (n < 0)
-	{
-		n = n * -1;
-	}
-
-	last = n % 10;
-
-	_putchar('0' + last);
-	return (last);
-}*/
