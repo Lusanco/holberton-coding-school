f54b4011725b53bd5dc28f52eaa56272739a9406
root <root@debianLaptop.debianLaptop>
2023-10-19 10:46:52 -0400

    Write a function that draws a straight line in the terminal.

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Write a function that
+ * draws a straight line in the terminal.
+ *
+ * @n: takes an int as argument
+ */
+
+void print_line(int n)
+{
+	int c = 0;
+
+	for (; c <= n; c++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
