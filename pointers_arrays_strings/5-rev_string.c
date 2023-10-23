ab87f2379318d0816ce4e2752f9248dd21109c27
root <root@debianLaptop.debianLaptop>
2023-10-23 15:45:10 -0400

    Write a function that reverses a string.

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * rev_string - Write a function that
+ * reverses a string.
+ *
+ * @s: takes a char as argument.
+ */
+
+void rev_string(char *s)
+{
+	int i, j, end;
+	char temp[25];
+
+	i = 0;
+	j = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	end = i;
+	while (i >= 0)
+	{
+		temp[j] = s[i];
+		i--;
+		j++;
+	}
+	i = 0;
+	j = 0;
+	while (i <= end)
+	{
+		s[i] = temp[j];
+		i++;
+		j++;
+	}
+}
---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - Write a function that
+ * prints every other character
+ * of a string, starting with
+ * the first character, followed
+ * by a new line.
+ *
+ * @str: takes a string as argument.
+ */
+
+void puts2(char *str)
+{
+	for 
