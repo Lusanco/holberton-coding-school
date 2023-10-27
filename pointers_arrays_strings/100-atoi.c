d72bb5df8fdcb57cf26e86d21eff0817af903020
Lusanco <93743011+Lusanco@users.noreply.github.com>
2023-10-26 20:28:02 -0400

    Fix, Practice, Reviewing

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <limits.h>
 
 /**
  * _atoi - Convert a string to an integer.
@@ -7,32 +9,40 @@
  *
  * Return: The integer value or 0 if no valid integer is found.
  */
+
 int _atoi(char *s)
 {
-    int i = 0, res = 0;
-    int sig = 1;
-    int found_digit = 0;
+	int i = 0, res = 0, sig = 1;
+	int found_digit = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-' && !found_digit)
-            sig *= -1;
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            found_digit = 1;
-            if (res > (2147483647 - (s[i] - '0')) / 10)
-            {
-                if (sig == 1)
-                    return 2147483647;
-                else
-                    return -2147483648;
-            }
-            res = res * 10 + (s[i] - '0');
-        }
-        else if (found_digit)
-            break;
-        i++;
-    }
-
-    return res * sig;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-' && !found_digit)
+		{
+			sig *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			if (res > (INT_MAX -
+			(s[i] - '0')) / 10)
+			{
+				if (sig == 1)
+				{
+					return (INT_MAX);
+				}
+				else
+				{
+					return (INT_MIN);
+				}
+			}
+			res = res * 10 + (s[i] - '0');
+		}
+		else if (found_digit)
+		{
+			break;
+		}
+		i++;
+	}
+	return (res * sig);
 }
