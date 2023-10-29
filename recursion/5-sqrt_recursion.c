58f9aa001595c178f5f03238f902b9effa6c53fc
Lusanco <93743011+Lusanco@users.noreply.github.com>
2023-10-29 14:09:29 -0400

    Fix 1, Removing loops, fixing more than x arguments.

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,45 +1,44 @@
 #include "main.h"
 
-/*
+/**
+ * squirt - Recursive helper function
+ * to find the square root
+ *
+ * @n: integer for which to calculate the square root
+ * @g: the current guess for the square root
+ *
+ * Return: the integer square root, or -1
+ * if there's no natural square root
+ */
+
+int squirt(int n, int g)
+{
+    if (g * g == n)
+    {
+        return g;
+    }
+    if (g * g > n)
+    {
+        return -1;
+    }
+    return squirt(n, g + 1);
+}
+
+/**
  * _sqrt_recursion - Function that returns
  * the natural square root of a number.
  *
  * @n: integer for which to calculate the square root
  *
- * Return: the integer square root,
- * or -1 if n is negative
+ * Return: the integer square root, or -1 if n is
+ * negative or if there's no natural square root
  */
 
 int _sqrt_recursion(int n)
 {
-	int start = 1;
-	int end = n;
-	int mid;
-
-	if (n < 0)
-	{
-		return (-1);
-	}
-	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	while (start <= end)
-	{
-		mid = start + (end - start) / 2;
-		if (mid * mid == n)
-		{
-			return (mid);
-		}
-		if (mid * mid < n)
-		{
-			start = mid + 1;
-		}
-		else
-		{
-			end = mid - 1;
-		}
-		
-	}
-	return (-1);
+    if (n < 0)
+    {
+        return -1;
+    }
+    return squirt(n, 1);
 }
