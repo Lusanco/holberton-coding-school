c206c6ab69c73537ba8f77bbd46737b371b3d43d
Lusanco <93743011+Lusanco@users.noreply.github.com>
2023-10-31 20:30:38 -0400

    Write a program that multiplies two numbers.

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**/
+
+int main(int argc, char *argv[])
+{
+	int total;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+	}
+	else if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		total = atoi(argv[1])
+			* atoi(argv[2]);
+		printf("%d\n", total);
+		return (0);
+	}
+}
