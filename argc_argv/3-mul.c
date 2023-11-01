ee4391c9c486ca10992ebd410aaa618f93b9005e
Lusanco <93743011+Lusanco@users.noreply.github.com>
2023-10-31 20:36:05 -0400

    Betty coding style, missing comment for main.

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**/
+/**
+ * main - multiplies two numbers.
+ *
+ * @argc: Number of arguments.
+ * @argv: Arguments.
+ *
+ * Return: 0 if succesful.
+ */
 
 int main(int argc, char *argv[])
 {
