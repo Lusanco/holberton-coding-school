708dc227832145beac9828a18ee7ff96137ca797
Lusanco <93743011+Lusanco@users.noreply.github.com>
2023-11-11 09:49:13 -0400

    Created my own strdup.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,45 @@
 #include "dog.h"
 
 /**
+ * _strdup - Custom string duplication function.
+ *
+ * @str: String to duplicate.
+ *
+ * Return: Pointer to the newly allocated
+ * duplicate string, or NULL if failure.
+ */
+
+char *my_strdup(char *str)
+{
+	char *dup_str;
+	int len = 0, i;
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	
+	dup_str = malloc((len + 1) *
+			sizeof(char));
+
+	if (dup_str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= len; i++)
+	{
+		dup_str[i] = str[i];
+	}
+
+	return (dup_str);
+}
+
+/**
  * new_dog - Function that creates a new dog.
  *
  * @name: name of dog struct
@@ -16,14 +55,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new_dog == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
 		return (NULL);
 	}
 
-	new_dog->name = strdup(name);
-	new_dog->owner = strdup(owner);
+	new_dog->name = my_strdup(name);
+	new_dog->owner = my_strdup(owner);
 
 	if (new_dog->name == NULL
 		|| new_dog->owner == NULL)
