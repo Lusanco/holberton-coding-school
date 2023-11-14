fbfacbc5c43aa67bfb6de2c05718fad33d765904
Lusanco <93743011+Lusanco@users.noreply.github.com>
2023-11-14 13:24:30 -0400

    C - Variadic functions, Github Setup

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,45 @@
+/**
+ * 0. Beauty is variable, ugliness
+ * is constant.
+ *
+ * Write a function that returns
+ * the sum of all its parameters.
+ *
+ * Prototype:
+ * int sum_them_all
+ * (const unsigned int n, ...);
+ *
+ * If n == 0, return 0
+ *
+ * Expected Output
+ *
+ * julien@ubuntu:~/0x0f. variadic
+ * functions$ gcc -Wall -pedantic
+ * -Werror -Wextra -std=gnu89
+ * 0-main.c 0-sum_them_all.c -o a
+ * julien@ubuntu:~/0x0f. variadic
+ * functions$ ./a
+ * 1122
+ * 500
+ * julien@ubuntu:~/0x0f. variadic
+ * functions$
+ */
+
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Function that
+ * returns the sum of all its
+ * parameters.
+ *
+ * @n: type of parameter to be
+ * passed. unsigned int.
+ *
+ * Return: The sum of all
+ * numbers passed.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
