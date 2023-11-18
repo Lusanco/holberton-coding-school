d1a5c2dc37734ba522193fed2e3a8b2369dd09e4
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-11-18 10:30:16 -0400

    style - add betty coding style

--- _printf.c
+++ _printf.c
@@ -1,37 +1,84 @@
 #include "main.h"
 
-int _printf(const char *format, ...)
+/**
+ * print_format - Helper function to print
+ * based on format specifier.
+ *
+ * @format: Format specifier character.
+ * @argList: va_list for variable arguments.
+ * @printedChars: Pointer to the variable
+ * holding the count of printed characters.
+ *
+ * Return: None.
+ */
+
+static void print_format(char format, va_list argList, int *printedChars)
 {
-	va_list args;
-	int count = 0, i;
-	char current_char, temp;
+	char c;
+	char *s;
 
-	va_start(args, format);
-
-	for (i = 0; format[i] != '\0'; i++)
+	switch (format)
 	{
-		current_char = format[i];
-
-		if (current_char == '%' && format[i + 1] != '\0')
+	case '%':
+		write(1, &format, 1);
+		(*printedChars)++;
+		break;
+	case 'c':
+		c = va_arg(argList, int);
+		write(1, &c, 1);
+		(*printedChars)++;
+		break;
+	case 's':
+		s = va_arg(argList, char*);
+		if (s != NULL)
 		{
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-					temp = va_arg(args, int);
-					count += write(1, &temp, 1);
-					break;
-				default:
-					count += write(1, "%", 1);
-					count += write(1, &format[i], 1);
-					break;
-			}
+			write(1, s, _strlen(s));
+			*printedChars += _strlen(s);
 		}
 		else
 		{
-			count += write(1, &current_char, 1);
+			write(1, "(null)", _strlen("(null)"));
+			*printedChars += _strlen("(null)");
 		}
+		break;
 	}
-	va_end(args);
-	return (count);
+}
+
+/**
+ * _printf - Replica of the standard library
+ * function of the same name.
+ *
+ * @format: Takes as argument a const char pointer.
+ *
+ * Return: If NULL, -1. Else, printedChars.
+ */
+
+int _printf(const char *format, ...)
+{
+	int printedChars = 0;
+	va_list argList;
+
+	va_start(argList, format);
+
+	if (format == NULL)
+		return (-1);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			printedChars++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			print_format(*format, argList, &printedChars);
+		}
+		format++;
+	}
+	va_end(argList);
+	return (printedChars);
 }
