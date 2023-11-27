76f7d41ee094b63aaa91d0858faa0e5dc8b6c06a
Lusanco <93743011+Lusanco@users.noreply.github.com>
2023-11-27 17:13:06 -0400

    Write a function that frees a list_t list.

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -23,13 +23,12 @@ typedef struct list_s
 int _putchar(char c);
 size_t _strlen(const char *s);
 char *_strdup(const char *str);
-void _free_list(list_t *head);
 size_t print_list(const list_t *h);
 void print_num(int n);
 void print_str(const char *str);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-void _free_list(list_t *head);
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
