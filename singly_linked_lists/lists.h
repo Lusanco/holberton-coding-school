fceab99ccfb26154e258d3a011c43038414a6a8b
Lusanco <93743011+Lusanco@users.noreply.github.com>
2023-11-27 16:56:42 -0400

    no effing idea why this one does not compile..., well it does but holbertons checkers says otherwise...

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -30,6 +30,6 @@ void print_str(const char *str);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+void _free_list(list_t *head);
 
 #endif /* LISTS_H */
