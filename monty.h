63f54e6437bacf72438cfc40d1143e584a104155
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-02 23:13:17 -0400

    code - re-added unused argument in pall.c

--- monty.h
+++ monty.h
@@ -50,7 +50,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack);
+void pall(stack_t **stack, unsigned int line_number);
 void command(char *opcode, stack_t **stack,
 			 unsigned int line_number, instruction_t opcodes[]);
 void free_stack(stack_t **stack);
--- pall.c
+++ pall.c
@@ -8,9 +8,10 @@
  * @line_number: line_number
  */
 
-void pall(stack_t **stack)
+void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
+	(void)line_number;
 
 	while (current != NULL)
 	{
