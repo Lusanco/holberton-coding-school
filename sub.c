bf79246a3a4568bca3522f6463f22ff9195b3529
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-03 13:51:32 -0400

    code - refactor/fix sub.c

--- sub.c
+++ sub.c
@@ -20,8 +20,9 @@ void sub(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	temp = *stack;
 	(*stack)->next->n -= (*stack)->n;
+	temp = *stack;
 	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
 	free(temp);
 }
