cba526f764904a47ba5846a4a1492c45620b2435
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-02 23:10:09 -0400

    code - removed unused argument in pall.c

--- pall.c
+++ pall.c
@@ -8,10 +8,9 @@
  * @line_number: line_number
  */
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack)
 {
 	stack_t *current = *stack;
-	(void)line_number;
 
 	while (current != NULL)
 	{
