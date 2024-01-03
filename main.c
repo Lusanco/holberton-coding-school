e5e0bbf89ae8ba3e7b1b8f4fd92adbf92825aa33
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-03 14:22:59 -0400

    code - change fprint for file name in main 3

--- main.c
+++ main.c
@@ -29,14 +29,14 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file %s\n", argv[1]);
+		fprintf(stderr, "USAGE: monty file\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
 	file = fopen(argv[1], "r");
 	if (!file)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file\n");
 		exit(EXIT_FAILURE);
 	}
 
