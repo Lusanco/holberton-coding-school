bd53b1cedca4d1302b0e7c618b8e2429c3acf0af
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-03 14:30:55 -0400

    code - ouptup message change

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
