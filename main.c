0125e11c33daaf2f79a5b89e53bc1c57a6e932fd
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-03 14:24:48 -0400

    code - reverting all tests to the original code

--- main.c
+++ main.c
@@ -29,14 +29,14 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n", argv[1]);
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
 	file = fopen(argv[1], "r");
 	if (!file)
 	{
-		fprintf(stderr, "Error: Can't open file\n");
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
