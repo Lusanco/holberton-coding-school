50b18bc310878eb578c8ce07c9d2b9674bf400fd
prodjohnper <JonathanPerez9743@gmail.com>
2024-01-04 11:44:38 -0400

    Add - Created nop opcode function

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - No operation.
+ *
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Line number in the monty file.
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
