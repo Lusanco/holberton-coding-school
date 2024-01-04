cb895a286f86fbae68d77ce3f96c1719e0a01de0
prodjohnper <JonathanPerez9743@gmail.com>
2024-01-04 13:22:23 -0400

    Update - Updated function comments to include correct information

--- sub.c
+++ sub.c
@@ -1,14 +1,11 @@
 #include "monty.h"
 
 /**
- * sub - Subtracts the top element of
- * the stack from the second top
- * element from stack
+ * sub - Function that subtracts the top element of
+ * the stack from the second top element from stack.
  *
- * @stack: Pointer to the top
- * of the stack
- * @line_number: Line number in the
- * monty file
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Line number in the monty file.
  */
 
 void sub(stack_t **stack, unsigned int line_number)
