3145b6ccd61161f6c53fecc26e825f2d7a89f2dd
prodjohnper <JonathanPerez9743@gmail.com>
2024-01-05 11:20:23 -0400

    Fix - Removed duplicated push opcode function prototype

--- monty.h
+++ monty.h
@@ -64,7 +64,6 @@ void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
-void push(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 
