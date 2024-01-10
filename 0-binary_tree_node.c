2140f371f9a7201b22df8596b9cfe0f311e7910e
Lusanco <lasc1026@gmail.com>
2024-01-10 13:13:00 -0400

    test - for NULL childs interaction

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,16 +11,21 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
 
-    if (new_node == NULL)
-        return (NULL);
+	new_node = (sizeof(binary_tree_t));
 
-    new_node->n = value;
+	if (new_node == NULL)
+		return (NULL);
 
-    if (parent == NULL)
-        new_node->parent = NULL;
-    else
-        new_node->parent = parent;
-    return (new_node);
+	new_node->n = value;
+/*new_node->left = NULL;*/
+/*new_node->right = NULL;*/
+
+	if (parent == NULL)
+		new_node->parent = NULL;
+	else
+		new_node->parent = parent;
+
+	return (new_node);
 }
