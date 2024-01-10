12915daebcd9f597b72aa8f7db65e30326962058
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-10 19:54:34 -0400

    fix -  return fix on binary tree preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,6 +9,9 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL || func == NULL)
+		return;
+
 	func(tree->n);
 	if (tree->left != NULL)
 		binary_tree_preorder(tree->left, func);
