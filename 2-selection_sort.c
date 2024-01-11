4063918a5252041c5b9742201d38517c27b9d9b4
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-11 12:45:38 -0400

    fix - betty coding stylet

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,24 +8,23 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
-    int temporary;
+	size_t i, j, min_index;
+	int temporary;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_index = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
-
-        if (min_index != i)
-        {
-            temporary = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temporary;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+		if (min_index != i)
+		{
+			temporary = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temporary;
+			print_array(array, size);
+		}
+	}
 }
