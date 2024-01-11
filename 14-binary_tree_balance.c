c1bd2a72bbf15996158db526328b06823cdab7ed
prodjohnper <JonathanPerez9743@gmail.com>
2024-01-10 22:19:30 -0400

    Update - Updated binary_tree_balance function to manage tree height inside it

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,8 +19,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = (tree->left) ? binary_tree_balance(tree->left) + 1 : 0;
+	right_height = (tree->right) ? binary_tree_balance(tree->right) + 1 : 0;
 
 	return (left_height - right_height);
 }
