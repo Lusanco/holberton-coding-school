8b44dc0aad4b9da906d075a05a500e4ee769d5a2
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-14 11:35:13 -0400

    fix/test - betty

--- add.c
+++ add.c
@@ -9,18 +9,17 @@
 
 void add(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-        cleanup(NULL, NULL, *stack);
-        exit(EXIT_FAILURE);
-    }
-
-    (*stack)->next->n += (*stack)->n;
-    temp = *stack;
-    *stack = (*stack)->next;
-    (*stack)->prev = NULL;
-    free(temp);
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		cleanup(NULL, NULL, *stack);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n += (*stack)->n;
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
 }
