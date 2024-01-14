4935475d164d5fb74e92f9e6ca0cbe119678c5a4
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-14 14:08:51 -0400

    test - implementation of helper funcs

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include "monty.h"
 
 /**
+ * usage_monty_ERR - Main helper 1 function.
+ *
+ * @argc: Arguments count.
+ */
+
+void usage_monty_ERR(int argc)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * cant_open_ERR - Main helper 1 function.
+ *
+ * @file: File name to open.
+ * @argv: argv.
+ * @line: line.
+ * @stack: stack.
+ */
+
+void cant_open_ERR(FILE *file, char *argv[], char *line, stack_t *stack)
+{
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		cleanup(file, line, stack);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
  * main - Main function.
  *
  * @argc: Arguments count.
@@ -19,56 +53,27 @@ int main(int argc, char *argv[])
 	ssize_t read;
 	unsigned int line_number = 0;
 	instruction_t opcodes[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"sub", sub},
-		{"add", add},
-		{"nop", nop},
-		{"div", div_func},
-		{"mul", mul_func},
-		{"mod", mod_func},
-		{NULL, NULL}};
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-
+		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap},
+		{"sub", sub}, {"add", add}, {"nop", nop}, {"div", div_func},
+		{"mul", mul_func}, {"mod", mod_func}, {NULL, NULL}};
+	usage_monty_ERR(argc);
 	file = fopen(argv[1], "r");
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		cleanup(file, line, stack);
-		exit(EXIT_FAILURE);
-	}
-
+	cant_open_ERR(file, *argv, line, stack);
 	while ((read = getline(&line, &len, file)) != -1)
 	{
 		line_number++;
 		if (line[0] == '#' || (line[0] == ' ' && line[1] == '#'))
 			continue;
-
 		if (line[0] == '\n' || (line[0] == ' ' && line[1] == '\n'))
 			continue;
-
 		opcode = strtok(line, " \t\n$");
-
 		if (opcode != NULL && strcmp(opcode, "#") != 0)
 		{
-			if (strcmp(opcode, "push") != 0 &&
-				strcmp(opcode, "pall") != 0 &&
-				strcmp(opcode, "pint") != 0 &&
-				strcmp(opcode, "pop") != 0 &&
-				strcmp(opcode, "swap") != 0 &&
-				strcmp(opcode, "add") != 0 &&
-				strcmp(opcode, "sub") != 0 &&
-				strcmp(opcode, "div") != 0 &&
-				strcmp(opcode, "mul") != 0 &&
-				strcmp(opcode, "mod") != 0 &&
+			if (strcmp(opcode, "push") != 0 && strcmp(opcode, "pall") != 0 &&
+				strcmp(opcode, "pint") != 0 && strcmp(opcode, "pop") != 0 &&
+				strcmp(opcode, "swap") != 0 && strcmp(opcode, "add") != 0 &&
+				strcmp(opcode, "sub") != 0 && strcmp(opcode, "div") != 0 &&
+				strcmp(opcode, "mul") != 0 && strcmp(opcode, "mod") != 0 &&
 				strcmp(opcode, "nop") != 0)
 			{
 				fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
