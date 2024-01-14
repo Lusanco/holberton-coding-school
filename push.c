646a50c25ea67a585c7144be68bd19f1030eab8d
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-14 11:20:06 -0400

    test - checking for only digits

--- push.c
+++ push.c
@@ -10,48 +10,40 @@
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node;
-	int value;
+	int value, i;
 	char *args = strtok(NULL, " \t\n$");
 
-	if (args == NULL || (!isdigit(*args) && *args != '-' && *args != '+'))
+	if (args == NULL || args[0] == '\0')
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		cleanup(NULL, NULL, *stack);
 		exit(EXIT_FAILURE);
 	}
 
-	if (*args == '-' || *args == '+')
-	{
-		args++;
-	}
+	for (int i = 0; args[i] != '\0'; i++)
+    {
+        if (!isdigit(args[i]) && !(i == 0 && (args[i] == '-' || args[i] == '+')))
+        {
+            fprintf(stderr, "L%u: usage: push integer\n", line_number);
+            cleanup(NULL, NULL, *stack);
+            exit(EXIT_FAILURE);
+        }
+    }
 
-	if (!isdigit(*args))
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		cleanup(NULL, NULL, *stack);
-		exit(EXIT_FAILURE);
-	}
+    value = atoi(args);
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        cleanup(NULL, NULL, *stack);
+        exit(EXIT_FAILURE);
+    }
 
-	value = atoi(args);
-	if (value == 0 && *args != '0')
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		cleanup(NULL, NULL, *stack);
-		exit(EXIT_FAILURE);
-	}
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		cleanup(NULL, NULL, *stack);
-		exit(EXIT_FAILURE);
-	}
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
 
-	new_node->n = value;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
-	*stack = new_node;
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
+    *stack = new_node;
 }
