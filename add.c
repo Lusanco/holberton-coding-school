1c31d808bf35a22ce966c612bb91f1b501552030
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-14 11:05:30 -0400

    fix - typo

--- add.c
+++ add.c
@@ -13,7 +13,7 @@ void add(stack_t **stack, unsigned int line_number)
 
     if (*stack == NULL || (*stack)->next == NULL)
     {
-        fprintf(stderr, "L%u: cant't add, stack too short\n", line_number);
+        fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
         cleanup(NULL, NULL, *stack);
         exit(EXIT_FAILURE);
     }
