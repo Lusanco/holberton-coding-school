bf9935de2e944db46904f5f3004ce57b0abc419d
Lusanco <lasc1026@gmail.com>
2024-01-17 10:40:53 -0400

    fix - typos/betty/names

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,16 +2,19 @@
 #include "search_algos.h"
 
 /**
- * binary_search -
+ * linear_search - function that searches for a value
+ * in an array of integers using the Linear search
+ * algorithm
  *
- * @array:
- * @size:
- * @value:
+ * @array: pointer to the first element of the array
+ * to search in
+ * @size: number of elements in array
+ * @value: is the value to search for
  *
- * Return:
+ * Return: On failure -1, On success value
  */
 
-int binary_search(int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
