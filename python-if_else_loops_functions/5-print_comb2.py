7f0ae2177a7cfb3773e17624f0299696b6c8162a
Lusanco <lasc1026@gmail.com>
2024-01-18 14:28:22 -0400

    code - 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 99):
-    print(f'{i} = {hex(i)}')
+for i in range(0, 100):
+    if i < 99:
+        print('{}'.format(i), end=", ")
+    else:
+        print('{}'.format(i))
