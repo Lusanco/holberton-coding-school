c310b0101350de7f91ba491c473196427e226ff2
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-22 10:48:17 -0400

    add - 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv) - 1
+    if argc == 0:
+        print("{} arguments.".format(argc))
+    elif argc == 1:
+        print("{} argument:".format(argc))
+        print("{}: {}".format(argc, argv[argc]))
+    else:
+        print("{} arguments:".format(argc))
+        for _ in range(1, argc + 1):
+            print("{}: {}".format(_, argv[_]))
