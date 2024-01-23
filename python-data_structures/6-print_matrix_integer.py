1364a39fdc004e27b96f63bea14249d138143aa3
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-22 22:51:09 -0400

    add - 6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for _ in range(len(row)):
+            if _ == len(row) - 1:
+                print("{:d}".format(row[_]), end="")
+            else:
+                print("{:d} ".format(row[_]), end="")
+        print()
