9ddacbe8e580724b990dfb430fe593257cc683ad
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-29 18:49:14 -0400

    test - for checkers with external code

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -52,15 +52,38 @@ guillaume@ubuntu:~/$
 """
 
 
-def safe_print_list_integers(my_list=None, x=0):
-    count = 0
+# def safe_print_list_integers(my_list=None, x=0):
+#     try:
+#         count = 0
+#         for i in range(x):
+#             if type(my_list[i]) is int:
+#                 print("{:d}".format(my_list[i]), end="")
+#                 count += 1
+#     except (TypeError, ValueError):
+#         raise
+#     finally:
+#         print()
+#     return count
+
+
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
     try:
+        count = 0
         for i in range(x):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-    except ValueError:
-        raise
-    finally:
+            else:
+                continue
         print()
-    return count
+        return count
+    except ValueError:
+        count = 0
+        for i in my_list:
+            if count == x:
+                break
+            else:
+                count += 1
+        print()
+        return count
