1b8334433cc6c80fa8335bfe7d4cdf85b8d83cf9
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-29 11:46:59 -0400

    style - pycodestyle

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,9 @@
 """
 1. Search and replace
 mandatory
-Write a function that replaces all occurrences of an element by another in a new list.
+Write a function that replaces all
+occurrences of an element by
+another in a new list.
 
 Prototype: def search_replace(my_list, search, replace):
 my_list is the initial list
@@ -23,7 +25,7 @@ print(my_list)
 guillaume@ubuntu:~/$ ./1-main.py
 [1, 89, 3, 4, 5, 4, 89, 1, 1, 4, 89]
 [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-guillaume@ubuntu:~/$ 
+guillaume@ubuntu:~/$
 """
 
 
