378b407d550f24af437eb6c4136c32a8764d87d3
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-29 12:09:38 -0400

    code - 3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,14 +2,16 @@
 """
 3. Present in both
 mandatory
-Write a function that returns a set of common elements in two sets.
+Write a function that returns a set of common
+elements in two sets.
 
 Prototype: def common_elements(set_1, set_2):
 You are not allowed to import any module
 
 guillaume@ubuntu:~/$ cat 3-main.py
 #!/usr/bin/python3
-common_elements = __import__('3-common_elements').common_elements
+common_elements =
+__import__('3-common_elements').common_elements
 
 set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" }
@@ -18,5 +20,10 @@ print(sorted(list(c_set)))
 
 guillaume@ubuntu:~/$ ./3-main.py
 ['C']
-guillaume@ubuntu:~/$ 
+guillaume@ubuntu:~/$
 """
+
+
+def common_elements(set_1, set_2):
+    new_set = set_1.intersection(set_2)
+    return new_set
