320233402247b47e94f15fc2ebbe50409342e023
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-29 17:38:14 -0400

    code - 4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -41,5 +41,26 @@ division by 0
 wrong type
 out of range
 [5.0, 0, 0, 2.0, 0]
-guillaume@ubuntu:~/$ 
+guillaume@ubuntu:~/$
 """
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            value_1 = my_list_1[i] if i < len(my_list_1) else 0
+            value_2 = my_list_2[i] if i < len(my_list_2) else 0
+            if isinstance(value_1, (int, float)) and isinstance(value_2, (int, float)):
+                if value_2 != 0:
+                    result.append(value_1 / value_2)
+                else:
+                    print("division by 0")
+                    result.append(0)
+            else:
+                print("wrong type")
+                result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+    return result
