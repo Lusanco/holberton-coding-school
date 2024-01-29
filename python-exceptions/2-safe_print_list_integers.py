736ca59280b6274ac85014f032e0a3e4a60a697e
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-29 17:54:50 -0400

    code - 2-safe_print_list_integers.py fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -60,7 +60,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
     except IndexError:
-        pass
+        raise
     finally:
         print()
-        return count
+    return count
