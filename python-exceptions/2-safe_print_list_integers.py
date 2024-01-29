3b2255c68e60a1a21026ef7bf4f7292a0162a3e8
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-29 18:50:52 -0400

    style - pycodestyle comments

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -52,21 +52,6 @@ guillaume@ubuntu:~/$
 """
 
 
-# def safe_print_list_integers(my_list=None, x=0):
-#     try:
-#         count = 0
-#         for i in range(x):
-#             if type(my_list[i]) is int:
-#                 print("{:d}".format(my_list[i]), end="")
-#                 count += 1
-#     except (TypeError, ValueError):
-#         raise
-#     finally:
-#         print()
-#     return count
-
-
-#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
