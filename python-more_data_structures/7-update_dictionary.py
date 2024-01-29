2204f8066a979d6a11d1ca936fc8cc88f07cb547
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-29 12:52:50 -0400

    code - 7-update_dictionary.py

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -8,13 +8,15 @@ Prototype: def update_dictionary(a_dictionary, key, value):
 key argument will be always a string
 value argument will be any type
 If a key exists in the dictionary, the value will be replaced
-If a key doesn’t exist in the dictionary, it will be created
+If a key doesnt exist in the dictionary, it will be created
 You are not allowed to import any module
 
 guillaume@ubuntu:~/$ cat 7-main.py
 #!/usr/bin/python3
-update_dictionary = __import__('7-update_dictionary').update_dictionary
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+update_dictionary =
+__import__('7-update_dictionary').update_dictionary
+print_sorted_dictionary =
+__import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
@@ -49,5 +51,10 @@ city: San Francisco
 language: Python
 number: 89
 track: Low level
-guillaume@ubuntu:~/$ 
+guillaume@ubuntu:~/$
 """
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
