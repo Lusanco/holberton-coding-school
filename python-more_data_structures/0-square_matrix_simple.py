61598b05205f26fe1bd2262a94244fdefae46cda
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-29 11:09:42 -0400

    code - 0-square_matrix_simple.py pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -36,9 +36,10 @@ guillaume@ubuntu:~/$
 
 
 def square_matrix_simple(matrix=[]):
-    squared_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    squared = [[0 for _ in range(len(matrix[0]))]
+               for _ in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            squared_matrix[i][j] = matrix[i][j] ** 2
+            squared[i][j] = matrix[i][j] ** 2
 
-    return squared_matrix
+    return squared
