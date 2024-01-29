eb979f6f9567e16b7e8369bb8d90f7c2b86ccb6f
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-29 17:18:46 -0400

    code - 3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,11 +2,13 @@
 """
 3. Integers division with debug
 mandatory
-Write a function that divides 2 integers and prints the result.
+Write a function that divides 2 integers
+and prints the result.
 
 Prototype: def safe_print_division(a, b):
 You can assume that a and b are integers
-The result of the division should print on the finally: section preceded by Inside result:
+The result of the division should print
+on the finally: section preceded by Inside result:
 Returns the value of the division, otherwise: None
 You have to use try: / except: / finally:
 You have to use "{}".format() to print the result
@@ -14,7 +16,8 @@ You are not allowed to import any module
 
 guillaume@ubuntu:~/$ cat 3-main.py
 #!/usr/bin/python3
-safe_print_division = __import__('3-safe_print_division').safe_print_division
+safe_print_division =
+__import__('3-safe_print_division').safe_print_division
 
 a = 12
 b = 2
@@ -31,5 +34,15 @@ Inside result: 6.0
 12 / 2 = 6.0
 Inside result: None
 12 / 0 = None
-guillaume@ubuntu:~/$ 
+guillaume@ubuntu:~/$
 """
+
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
