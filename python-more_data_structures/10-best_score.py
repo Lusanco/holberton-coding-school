019acb77024bf999f7dd27f73583583601b2470b
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-29 15:59:42 -0400

    code - 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""10. Best score
+"""
+10. Best score
 mandatory
-Write a function that returns a key with the biggest integer value.
+Write a function that returns a key with
+the biggest integer value.
 
 Prototype: def best_score(a_dictionary):
 You can assume that all values are only integers
@@ -13,7 +15,8 @@ guillaume@ubuntu:~/$ cat 10-main.py
 #!/usr/bin/python3
 best_score = __import__('10-best_score').best_score
 
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+a_dictionary = {'John': 12, 'Bob': 14,
+'Mike': 14, 'Molly': 16, 'Adam': 10}
 best_key = best_score(a_dictionary)
 print("Best score: {}".format(best_key))
 
@@ -23,5 +26,13 @@ print("Best score: {}".format(best_key))
 guillaume@ubuntu:~/$ ./10-main.py
 Best score: Molly
 Best score: None
-guillaume@ubuntu:~/$ 
+guillaume@ubuntu:~/$
 """
+
+
+def best_score(a_dictionary):
+    if a_dictionary:
+        best = max(a_dictionary, key=a_dictionary.get)
+        return best
+    else:
+        return None
