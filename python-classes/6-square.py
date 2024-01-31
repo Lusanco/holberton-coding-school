4de63a09eea93b31a4d05395224ab5b76f8a8004
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-01-31 19:33:08 -0400

    code - 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 6. Coordinates of a square
 mandatory
@@ -7,17 +8,25 @@ Write a class Square that defines a square by: (based on 5-square.py)
 Private instance attribute: size:
 property def size(self): to retrieve it
 property setter def size(self, value): to set it:
-size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
+size must be an integer, otherwise raise a TypeError exception with
+the message size must be an integer
+if size is less than 0, raise a ValueError exception with the message
+size must be >= 0
 Private instance attribute: position:
 property def position(self): to retrieve it
 property setter def position(self, value): to set it:
-position must be a tuple of 2 positive integers, otherwise raise a TypeError exception with the message position must be a tuple of 2 positive integers
-Instantiation with optional size and optional position: def __init__(self, size=0, position=(0, 0)):
-Public instance method: def area(self): that returns the current square area
-Public instance method: def my_print(self): that prints in stdout the square with the character #:
+position must be a tuple of 2 positive integers, otherwise raise a
+TypeError exception with the message position must be a tuple of 2
+positive integers
+Instantiation with optional size and optional position:
+def __init__(self, size=0, position=(0, 0)):
+Public instance method: def area(self): that returns the current
+square area
+Public instance method: def my_print(self): that prints in stdout
+the square with the character #:
 if size is equal to 0, print an empty line
-position should be use by using space - Don’t fill lines by spaces when position[1] > 0
+position should be use by using space - Don’t fill lines by spaces
+when position[1] > 0
 You are not allowed to import any module
 
 guillaume@ubuntu:~/$ cat 6-main.py
@@ -55,3 +64,50 @@ ___###$
 --$
 guillaume@ubuntu:~/$
 """
+
+
+class Square:
+    """Defines Square class w/ private size=0"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(i, int) and i >= 0 for i in value)
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__position[1]):
+            print()
+        for _ in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
