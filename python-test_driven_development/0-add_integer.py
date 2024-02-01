015b373112f1c5165be7f08acfa04e9f65dac232
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-01 11:12:03 -0400

    updated/test - explicit changes to code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -21,11 +21,13 @@ def add_integer(a, b=98):
     Returns:
     int: The addition of a and b.
     """
-    # if not a:
-    #     raise TypeError("a must be an integer")
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    return int(a) + int(b)
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
