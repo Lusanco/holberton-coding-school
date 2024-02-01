4f50344166424a95a817d717686b7066331569e3
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-01 11:20:51 -0400

    test - for chekers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Module Name: 0-add_integer
 Description: Adds two integers
@@ -9,16 +8,6 @@ Date: Feb 01 2024
 
 
 def add_integer(a, b=98):
-    """
-    Function Name: add_integer
-    Description: Adds two integers
-    Parameters:
-    - a (int or float): The first integer.
-    - b (int or float): The second integer. Default is 98.
-    Returns:
-    int: The addition of a and b.
-    """
-
     if not a:
         raise TypeError("a must be an integer")
     if type(a) is not int and type(a) is not float:
