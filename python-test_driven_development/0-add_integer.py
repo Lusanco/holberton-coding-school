820633fddb98d44eb29d86b6dbe3ee9bfc2f55fd
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-01 11:06:42 -0400

    test -  removed code on script

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -21,11 +21,10 @@ def add_integer(a, b=98):
     Returns:
     int: The addition of a and b.
     """
-    if not a:
-        raise TypeError("a must be an integer")
+    # if not a:
+    #     raise TypeError("a must be an integer")
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
