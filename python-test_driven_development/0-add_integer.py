0561619b4b0ff634423e72ee388e0f11b4e60a61
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-01 11:04:34 -0400

    test - for checkers add when no args are passed

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,6 +10,19 @@ Date: Feb 01 2024
 
 
 def add_integer(a, b=98):
+    """
+    Function Name: add_integer
+    Description: Adds two integers
+
+    Parameters:
+    - a (int or float): The first integer.
+    - b (int or float): The second integer. Default is 98.
+
+    Returns:
+    int: The addition of a and b.
+    """
+    if not a:
+        raise TypeError("a must be an integer")
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
