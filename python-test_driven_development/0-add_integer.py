ff0e158f958d4359e9bce5a457ed6b2d643ca750
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-01 14:31:04 -0400

    update - 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,7 +11,7 @@ def add_integer(a, b=98):
     - a (int or float): The first integer.
     - b (int or float): The second integer. Default is 98."""
 
-    if a is None or b is None:
+    if a == None or b == None:
         raise TypeError(
             "unsupported operand type(s) \
                         for +: 'NoneType' and 'int'"
