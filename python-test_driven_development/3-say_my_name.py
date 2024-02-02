9da4fb3f8bfbef5a7a70dd72d571b48c163b6a18
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-02 17:45:27 -0400

    test - for checkers

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,26 +2,20 @@
 """
 Module Name: 3-say_my_name
 Description: prints name and last name
-Authors: Lusanco, Ltoro9, Antoniofdjs Date: Feb 01 2024
+Authors: Lusanco, Ltoro9, Antoniofdjs, Livanhernandez Date: Feb 01 2024
 """
 
 
 def say_my_name(first_name, last_name=""):
     """Description: prints first name and last name
     - first_name (str): The name string
-    - last_name (str): The last name, default is ''"""
+    - last_name (str): The last name, default is \" \" """
 
-    error_a = "first_name must be a string"
-    error_b = "last_name must be a string"
+    first = "first_name must be a string"
+    last = "last_name must be a string"
 
-    if type(first_name) is not str:
-        raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+    if not isinstance(first_name, str):
+        raise TypeError(first)
+    if not isinstance(last):
         raise TypeError("last_name must be a string")
-
-    if first_name != "" and last_name != "":
-        print("My name is {} {}".format(first_name, last_name))
-    elif first_name != "":
-        print("My name is {}".format(first_name))
-    else:
-        print("My name is {}".format(last_name))
+    print(f"My name is {first_name} {last_name}")
