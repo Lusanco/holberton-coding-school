7b5a9215d44b2fe6f9c9601809bddba13592cce1
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-02 17:38:20 -0400

    test - for checkers

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,7 +2,7 @@
 """
 Module Name: 2-matrix_divided
 Description: Divides all elements of a matrix
-Authors: Lusanco, Ltoro9, Antoniofdjs Date: Feb 01 2024
+Authors: Lusanco, Ltoro9, Antoniofdjs, Livanhernandez Date: Feb 01 2024
 """
 
 
@@ -13,28 +13,23 @@ def matrix_divided(matrix, div):
 
     mtx = "matrix must be a matrix (list of lists) of integers/floats"
     siz = "Each row of the matrix must have the same size"
-
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
+    result = []
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
-    temp = len(matrix[0])
-    for row in matrix:
-        if not isinstance(row, list):
-            raise TypeError(mtx)
-        if temp != len(row):
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for lists in matrix:
+        if len(lists) != len(matrix[0]):
             raise TypeError(siz)
-        temp = len(row)
-
-    new_matrix = [row[:] for row in matrix]
-
-    for row in range(len(matrix)):
-        for column in range(len(matrix[row])):
-            if not isinstance(matrix[row][column], (int, float)):
+        inner = []
+        for elem in lists:
+            if not isinstance(elem, (int, float)):
                 raise TypeError(mtx)
-            result = round(matrix[row][column] / div, 2)
-            new_matrix[row][column] = result
+            else:
+                inner.append(round(elem / div, 2))
+        result.append(inner)
 
-    return new_matrix
+    return result
