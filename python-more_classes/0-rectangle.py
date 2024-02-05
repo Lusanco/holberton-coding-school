d47cc5a79ec9bd81eec8e4049545573ba707ce61
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-05 11:43:28 -0400

    code - replaced text comments

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
 """
-0. Simple rectangle
-mandatory
-Write an empty class Rectangle that defines a rectangle:
-
-You are not allowed to import any module
-
-guillaume@ubuntu:~/$ cat 0-main.py
-#!/usr/bin/python3
-Rectangle = __import__('0-rectangle').Rectangle
-
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
-
-guillaume@ubuntu:~/$ ./0-main.py
-<class '0-rectangle.Rectangle'>
-{}
-guillaume@ubuntu:~/$
+Module Name: 0-rectangle
+Description: Empty Rectangle Class
+Authors: Lusanco
 """
 
 
 class Rectangle:
+    """Empty Rectangle Class"""
+
     pass
