b17908fa9a76a2ca62e4a6a5935e9a68d785f1af
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-05 14:43:05 -0400

    code - 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,106 +1,55 @@
 #!/usr/bin/python3
 """
-3. String representation
-mandatory
-Write a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)
-
-Private instance attribute: width:
-property def width(self): to retrieve it
-property setter def width(self, value): to set it:
-width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
-if width is less than 0, raise a ValueError exception with the message width must be >= 0
-Private instance attribute: height:
-property def height(self): to retrieve it
-property setter def height(self, value): to set it:
-height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
-if height is less than 0, raise a ValueError exception with the message height must be >= 0
-Instantiation with optional width and height: def __init__(self, width=0, height=0):
-Public instance method: def area(self): that returns the rectangle area
-Public instance method: def perimeter(self): that returns the rectangle perimeter:
-if width or height is equal to 0, perimeter has to be equal to 0
-print() and str() should print the rectangle with the character #: (see example below)
-if width or height is equal to 0, return an empty string
-You are not allowed to import any module
-guillaume@ubuntu:~/$ cat 3-main.py
-#!/usr/bin/python3
-Rectangle = __import__('3-rectangle').Rectangle
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
-
-guillaume@ubuntu:~/$ ./3-main.py
-Area: 8 - Perimeter: 12
-##
-##
-##
-##
-<3-rectangle.Rectangle object at 0x7f92a75a2eb8>
---
-##########
-##########
-##########
-<3-rectangle.Rectangle object at 0x7f92a75a2eb8>
-guillaume@ubuntu:~/$
-3. String representation
-mandatory
-Write a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)
-
-Private instance attribute: width:
-property def width(self): to retrieve it
-property setter def width(self, value): to set it:
-width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
-if width is less than 0, raise a ValueError exception with the message width must be >= 0
-Private instance attribute: height:
-property def height(self): to retrieve it
-property setter def height(self, value): to set it:
-height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
-if height is less than 0, raise a ValueError exception with the message height must be >= 0
-Instantiation with optional width and height: def __init__(self, width=0, height=0):
-Public instance method: def area(self): that returns the rectangle area
-Public instance method: def perimeter(self): that returns the rectangle perimeter:
-if width or height is equal to 0, perimeter has to be equal to 0
-print() and str() should print the rectangle with the character #: (see example below)
-if width or height is equal to 0, return an empty string
-You are not allowed to import any module
-
-guillaume@ubuntu:~/$ cat 3-main.py
-#!/usr/bin/python3
-Rectangle = __import__('3-rectangle').Rectangle
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
-
-guillaume@ubuntu:~/$ ./3-main.py
-Area: 8 - Perimeter: 12
-##
-##
-##
-##
-<3-rectangle.Rectangle object at 0x7f92a75a2eb8>
---
-##########
-##########
-##########
-<3-rectangle.Rectangle object at 0x7f92a75a2eb8>
-guillaume@ubuntu:~/$
+Module Name: 3-rectangle
+Description: Defines a Rectangle, returns area, perimeter and rectangle #
+Authors: Lusanco
 """
+
+
+class Rectangle:
+    """Defines a Rectangle, returns area, perimeter and rectangle #"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.height == 0 or self.width == 0:
+            return 0
+        else:
+            return (self.__height * 2) + (self.width * 2)
+
+    def __str__(self) -> str:
+        rect_str = ""
+        if self.width == 0 or self.height == 0:
+            return rect_str
+        for _ in range(self.height):
+            rect_str += "#" * self.width + "\n"
+        return rect_str.strip()
