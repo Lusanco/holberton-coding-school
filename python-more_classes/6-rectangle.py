7630012f787850d32d903d0191e780ae3c8aa8ff
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-05 18:38:56 -0400

    code - 6-rectangular.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,50 +1,66 @@
 #!/usr/bin/python3
 """
-6. How many instances
-mandatory
-Write a class Rectangle that defines a rectangle by: (based on 5-rectangle.py)
-
-Private instance attribute: width:
-property def width(self): to retrieve it
-property setter def width(self, value): to set it:
-width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
-if width is less than 0, raise a ValueError exception with the message width must be >= 0
-Private instance attribute: height:
-property def height(self): to retrieve it
-property setter def height(self, value): to set it:
-height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
-if height is less than 0, raise a ValueError exception with the message height must be >= 0
-Public class attribute number_of_instances:
-Initialized to 0
-Incremented during each new instance instantiation
-Decremented during each instance deletion
-Instantiation with optional width and height: def __init__(self, width=0, height=0):
-Public instance method: def area(self): that returns the rectangle area
-Public instance method: def perimeter(self): that returns the rectangle perimeter:
-if width or height is equal to 0, perimeter has to be equal to 0
-print() and str() should print the rectangle with the character #:
-if width or height is equal to 0, return an empty string
-repr() should return a string representation of the rectangle to be able to recreate a new instance by using eval()
-Print the message Bye rectangle... (... being 3 dots not ellipsis) when an instance of Rectangle is deleted
-You are not allowed to import any module
-
-guillaume@ubuntu:~/$ cat 6-main.py
-#!/usr/bin/python3
-Rectangle = __import__('6-rectangle').Rectangle
-
-my_rectangle_1 = Rectangle(2, 4)
-my_rectangle_2 = Rectangle(2, 4)
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_1
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_2
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-
-guillaume@ubuntu:~/$ ./6-main.py
-2 instances of Rectangle
-Bye rectangle...
-1 instances of Rectangle
-Bye rectangle...
-0 instances of Rectangle
-guillaume@ubuntu:~/$
+Module Name: 6-rectangle
+Description: Defines a Rectangle, returns
+area, perimeter and rectangle #
+Authors: Lusanco
 """
+
+
+class Rectangle:
+    """Defines a Rectangle, returns area, perimeter and rectangle #"""
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.height == 0 or self.width == 0:
+            return 0
+        else:
+            return (self.__height * 2) + (self.width * 2)
+
+    def __str__(self) -> str:
+        rect_str = ""
+        if self.width == 0 or self.height == 0:
+            return rect_str
+        for _ in range(self.height):
+            rect_str += "#" * self.width + "\n"
+        return rect_str.strip()
+
+    def __repr__(self) -> str:
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        print(f"Bye rectangle...")
+        Rectangle.number_of_instances -= 1
