a2553aaca19e29ad2355a4ed5d610e1c35564b2c
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-07 22:21:56 -0400

    code - 5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Module Name: 5-base_geometry
+Description: empty class BaseGeometry
+Authors: Lusanco
+"""
+
+
+class BaseGeometry:
+    """empty class BaseGeometry."""
+
+    pass
