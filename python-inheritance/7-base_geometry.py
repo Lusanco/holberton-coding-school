33db85672a78d4b7ab675eb0141b87cc84a4c818
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-07 22:38:30 -0400

    code/doctest - 7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+"""
+Module Name: 7-base_geometry
+Description: class BaseGeometry
+(based on 6-base_geometry.py)
+Authors: Lusanco
+"""
+
+
+class BaseGeometry:
+    """Public instance method that raises an Exception && Public instance method that validates value"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise TypeError("{} must be greater than 0".format(name))
