18aedf17471dbd42f4de1fdc05a56901b9d185d3
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-07 21:29:30 -0400

    code - 0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Module Name: 0-lookup
+Description: returns the list of available
+attributes and methods of an object
+Authors: Lusanco
+"""
+
+
+def lookup(obj):
+    return dir(obj)
