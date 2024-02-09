6746dcfd324233f08a75b150046e6b10d28a2bdd
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-08 21:46:10 -0400

    code - 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""
+Module Name: 0-read_file.py
+Description: reads a text file (UTF8)
+and prints it to stdout
+Authors: Lusanco
+"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and
+    prints it to stdout"""
+
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
