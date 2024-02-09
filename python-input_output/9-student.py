5593f308aad8362d8c142c9f8e7a9bfb8ee2d85a
Lusanco <lusanco@gmail.com>
2024-02-09 11:23:43 -0400

    code - 9-students.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Module: 9-student
+Descrip: class Student that defines a student
+by the Public instance attributes:
+first_name, last_name, age
+Authors: Lusanco
+"""
+
+
+class Student:
+    """class Student that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
