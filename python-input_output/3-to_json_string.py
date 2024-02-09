7d4d75cb81d140352a7524b6abe908cf5be19b4e
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-08 22:06:56 -0400

    code - 3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""
+Module Name: 3-json_string.py
+Description: returns the JSON
+representation of an object (string)
+Authors: Lusanco
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation
+    of an object (string)"""
+
+    return json.dumps(my_obj)
