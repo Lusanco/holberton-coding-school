9059d7a9ba62a43e67886d2f36ff8c571fb974b8
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-08 22:29:38 -0400

    code - 6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Module Name: 6-load_from_json_file.py
+Description: creates an Object from a “JSON file”
+Authors: Lusanco
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a “JSON file”"""
+    with open(filename, "r") as file:
+        return json.load(file)
