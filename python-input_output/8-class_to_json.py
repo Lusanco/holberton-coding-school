892dcdf65036a9757773dea5fd18fdfb9f7c95f7
Lusanco <lusanco@gmail.com>
2024-02-09 11:10:48 -0400

    code - 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Module: 8-class_to_json
+Descrip: returns the dictionary description
+with simple data structure (list, dictionary,
+string, integer and boolean) for JSON
+serialization of an object
+Authors: Lusanco
+"""
+
+
+def class_to_json(obj):
+    return obj.__dict__
