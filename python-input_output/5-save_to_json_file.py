6b876a73500d86d99c5c0a53a5ca37b84e596744
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-08 22:22:31 -0400

    code - 5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Module Name: 5-save_to_json_file.py
+Description: writes an Object to a text
+file, using a JSON representation
+Authors: Lusanco
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text
+    file, using a JSON representation"""
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
