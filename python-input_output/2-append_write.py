c780d4185b29119f6dab32754e24664a19df9011
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-08 22:01:05 -0400

    code - 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""
+Module Name: 2-append_write.py
+Description: appends a string at the
+end of a text file (UTF8) and returns
+the number of characters added
+Authors: Lusanco
+"""
+
+
+def append_write(filename="", text=""):
+    """appends a string at the
+    end of a text file (UTF8) and returns
+    the number of characters added"""
+
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
