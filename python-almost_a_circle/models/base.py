6ba0c4c315e1c0c63e8fc5467b9cdc5c92b332a1
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-12 13:38:54 -0400

    code - base.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Module: base
+Descrip: base class
+Authors: Lusanco
+"""
+
+
+class Base:
+    """base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
