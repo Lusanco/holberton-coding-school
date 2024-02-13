ec114cc15a762bbb47579b1eea4c278554f56949
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-12 20:13:14 -0400

    code - update Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -27,4 +27,9 @@ class Square(Rectangle):
         self.height = value
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id,
+                self.x,
+                self.y,
+                self.width
+                )
