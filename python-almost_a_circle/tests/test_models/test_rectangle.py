f3cb18033d467b94bda3b312984c9b9e5c6fa977
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-16 19:34:24 -0400

    code - minor test for checkers

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -19,6 +19,15 @@ class TestRectangle(unittest.TestCase):
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
 
+        r4 = Rectangle(1, 2)
+        self.assertEqual(r4.id, 3)
+
+        r5 = Rectangle(1, 2, 3)
+        self.assertEqual(r5.id, 4)
+
+        r6 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r6.id, 5)
+
     def test_invalid_inputs(self):
         with self.assertRaises(TypeError):
             Rectangle(10, "2")
