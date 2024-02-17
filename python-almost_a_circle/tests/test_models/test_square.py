1519d09562f0c8b558a93fc096bb37a88c3258be
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-17 13:02:37 -0400

    code - square 2

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -14,11 +14,22 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s1.id, 1)
         self.assertEqual(s1.size, 1)
 
-        s2 = Square(1, 2, 3, 4)
-        self.assertEqual(s2.id, 4)
+        s2 = Square(1, 2)
+        self.assertEqual(s2.id, 2)
         self.assertEqual(s2.size, 1)
         self.assertEqual(s2.x, 2)
-        self.assertEqual(s2.y, 3)
+
+        s3 = Square(1, 2, 3)
+        self.assertEqual(s3.id, 3)
+        self.assertEqual(s3.size, 1)
+        self.assertEqual(s3.x, 2)
+        self.assertEqual(s3.y, 3)
+
+        s4 = Square(1, 2, 3, 4)
+        self.assertEqual(s4.id, 4)
+        self.assertEqual(s4.size, 1)
+        self.assertEqual(s4.x, 2)
+        self.assertEqual(s4.y, 3)
 
         with self.assertRaises(TypeError):
             Square("1")
