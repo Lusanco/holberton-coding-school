0937cf75063ce28a6c2b68eba40fe918de857980
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-03-07 21:34:55 -0400

    sql - Always a name -- Use db removed

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Script that creates the table force_name on your MySQL server
-USE hbtn_0d_2;
+-- USE hbtn_0d_2;
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
