d1bdf4d211320f43ce36996b8a4e82566c05d6d4
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-03-09 12:12:43 -0400

    sql - Unique ID

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,1 +1,6 @@
 -- Script that creates the table unique_id on your MySQL server
+-- USE db; # first step select db
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT NOT NULL DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
