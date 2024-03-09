728a266dae11625e42d68f5236fd62bebede99f1
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-03-09 15:06:08 -0400

    sql - Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,4 @@
 -- Script that lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states
-ON cities.states_id = states.id
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.states_id = states.id
 ORDER BY cities.id ASC; 
