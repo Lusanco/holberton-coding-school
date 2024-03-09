a90aa0fe3fa04e4af652afa714e66c0fb34057e3
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-03-09 11:30:51 -0400

    sql - ID can not be null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,1 +1,6 @@
 -- Script that creates the table id_not_null on your MySQL server
+-- USE db; # first step select db
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256)
+);
