9138447429c5e9db74008595e1fe769b12e4b70d
Lusanco <lusanco@gmail.com>
2024-03-11 18:56:40 -0400

    py - add 7-model_state_fetch.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,40 @@
 #!/usr/bin/python3
+
+"""
+Module: Model State
+Author: Lusanco
+Descri: Script that lists all State
+objects from the database hbtn_0e_6_usa
+"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    dburl = 'mysql+mysqldb://{}:{}@localhost/{}'
+    dburl = dburl.format(
+            argv[1],
+            argv[2],
+            argv[3])
+    engine = create_engine(
+        dburl,
+        pool_pre_ping=True)
+
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+
+if __name__ == "__main__":
+    main()
