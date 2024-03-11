f88ef9db4005bc73bf153e1ea8d9c4c7a0610577
Lusanco <lusanco@gmail.com>
2024-03-11 14:57:09 -0400

    refactor - coding in different approach

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,38 +10,45 @@ states from the database hbtn_0e_0_usa.
 
 # Development modules
 import MySQLdb
-import sys
+from sys import argv
 
 # Debugging module
 from icecream import ic
 
-# if name == main managing
-# sys arguments (from terminal)
+def main():
+    """Main Program"""
+    
+    
+    # varibale arguments
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+
+    # Connect to the server (MySQL)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database)
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Run the SQL statement
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    # Close connection
+    cur.close()
+    db.close()
+
+# Prevents execution when imported
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-# Connect to the server (MySQL)
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=database)
-
-# Create a cursor object
-cur = db.cursor()
-
-# Run the SQL statement
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-# Fetch all the rows
-rows = cur.fetchall()
-
-for row in rows:
-    print(row)
-
-# Close connection
-cur.close()
-db.close()
+        main()
