4cfa74d9348efdda150f0849610a014850d190d9
Lusanco <lusanco@gmail.com>
2024-03-11 20:18:00 -0400

    py - add 10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+
+"""
+Module: Model State My Get
+Author: Lusanco
+Descri: Script that prints the State
+object with the name passed as argument
+from the database hbtn_0e_6_usa
+"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    dburl = 'mysql+mysqldb://{}:{}@localhost/{}'
+    dburl = dburl.format(
+            argv[1],
+            argv[2],
+            argv[3])
+    engine = create_engine(
+        dburl,
+        pool_pre_ping=True)
+
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_name = argv[4]
+    state = session.query(State)
+    state = state.filter(State.name == state_name)
+    state = state.first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
