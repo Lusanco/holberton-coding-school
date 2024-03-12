c22f9ff7b57686d4054e498853bf71db8fd5857d
Lusanco <lusanco@gmail.com>
2024-03-11 21:29:20 -0400

    py - model city files

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,44 @@
 #!/usr/bin/python3
+
+"""
+Module: Model City Fetch By State
+Author: Lusanco
+Descri: Script that prints all City
+objects from the database hbtn_0e_14_usa
+"""
+
+
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    dburl = 'mysql+mysqldb://{}:{}@localhost/{}'
+    dburl = dburl.format(
+            argv[1],
+            argv[2],
+            argv[3])
+    engine = create_engine(
+        dburl,
+        pool_pre_ping=True)
+
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City, State)
+    cities = cities.join(State)
+    cities = cities.order_by(City.id).all()
+
+    for city, state in cities:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+
+"""
+Module: Model City
+Author: Lusanco
+Descri: Definition of the City class
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """
+    City class inherits Base
+
+    Attributes:
+        id (int): Unique identifier
+        (for the city, primary key)
+        name (str): Name of the city
+        (max length of 128 characters)
+        state_id (int): Foreign key
+        referencing the state_id
+        in the states table
+    """
+
+    __tablename__ = 'cities'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True)
+
+    name = Column(
+        String(128),
+        nullable=False)
+    
+    state_id = Column(
+        Integer,
+        ForeignKey('states.id'),
+        nullable=False)
