132213cc070d1a8c24a837636f38084f2f4992b5
Lusanco <lusanco@gmail.com>
2024-03-11 20:05:27 -0400

    py - 9-model_state_filter_a.py

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python3
+
+"""
+Module: Model State Fetch All
+Author: Lusanco
+Descri: Script that lists all State
+objects from the database hbtn_0e_6_usa
+"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    dburl = 'mysql+mysqldb://{}:{}@localhost/{}'
+    dburl = dburl.format(
+            argv[1],
+            argv[2],
+            argv[3])
+    engine = create_engine(
+        dburl,
+        pool_pre_ping=True)
+
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State)
+    states = states.filter(State.name.like('%a%'))
+    states = states.order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
