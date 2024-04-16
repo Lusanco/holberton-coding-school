f6ff72c92f0367ee4a382724cc8143f934ad42e0
Lusanco <lasc1026@gmail.com>
2024-04-15 23:58:54 -0400

    testing - other code 2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,57 +4,6 @@
 holbertonschool_backend/api
 """
 
-import requests
-import sys
-
-
-# def data_from_api():
-#     """Gather data from an API by input ID"""
-
-#     employee_id = int(sys.argv[1])
-
-#     base_url = "https://jsonplaceholder.typicode.com"
-#     # fmt: off
-#     user_url = "{}/users/{}".format(
-#         base_url, employee_id)
-#     todo_url = "{}/todos?userId={}".format(
-#         base_url, employee_id)
-#     # fmt: on
-
-#     user_res = requests.get(user_url)
-#     todo_res = requests.get(todo_url)
-
-#     employee_name = user_res.json()["name"]
-#     tasks = todo_res.json()
-#     total_tasks = len(tasks)
-
-#     done_tasks = []
-#     for task in tasks:
-#         if task["completed"]:
-#             done_tasks.append(task)
-
-#     num_done_tasks = len(done_tasks)
-
-#     print(
-#         "Employee {} is done with tasks({}/{}):".format(
-#             employee_name, num_done_tasks, total_tasks
-#         )
-#     )
-
-#     for task in done_tasks:
-#         print(f"\t {task['title']}")
-
-
-# if __name__ == "__main__":
-#     data_from_api()
-
-
-#!/usr/bin/python3
-"""
-    Api REST
-"""
-
-
 import json
 import requests
 from sys import argv
