9cd3aafd453c81813d45d8410f55c855d63c6fb8
Lusanco <lasc1026@gmail.com>
2024-04-15 23:56:41 -0400

    testing - other code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -49,43 +49,58 @@ import sys
 #     data_from_api()
 
 
-def data_from_api():
-    """Gather data from an API by input ID"""
+#!/usr/bin/python3
+"""
+    Api REST
+"""
 
-    employee_id = int(sys.argv[1])
 
-    base_url = "https://jsonplaceholder.typicode.com"
-    # fmt: off
-    user_url = "{}/users/{}".format(
-        base_url, employee_id)
-    todo_url = "{}/todos?userId={}".format(
-        base_url, employee_id)
-    # fmt: on
+import json
+import requests
+from sys import argv
 
-    user_res = requests.get(user_url)
-    todo_res = requests.get(todo_url)
 
-    EMPLOYEE_NAME = user_res.json()["name"]
-    tasks = todo_res.json()
-    TOTAL_NUMBER_OF_TASKS = len(tasks)
+def get_employee(id=None):
+    """
+    using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress.
+    """
+    # check if argv[1] is a number int, it means we are using argv
+    if len(argv) > 1:
+        try:
+            id = int(argv[1])
+        except ValueError:
+            pass
+            return
 
-    done_tasks = []
-    for task in tasks:
-        if task["completed"]:
-            done_tasks.append(task)
-
-    NUMBER_OF_DONE_TASKS = len(done_tasks)
-
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
+    if isinstance(id, int):
+        user = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
+        to_dos = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos/?userId={id}"
         )
-    )
 
-    for task in done_tasks:
-        TASK_TITLE = task["title"]
-        print("\t {}".format(TASK_TITLE))
+        if to_dos.status_code == 200 and user.status_code == 200:
+            user = json.loads(user.text)
+            to_dos = json.loads(to_dos.text)
+
+            total_tasks = len(to_dos)
+            tasks_completed = 0
+            titles_completed = []
+
+            for to_do in to_dos:
+                if to_do["completed"] is True:
+                    tasks_completed += 1
+                    titles_completed.append(to_do["title"])
+
+            tasks_completed = len(titles_completed)
+
+            print(
+                f"Employee {user['name']} is done with tasks\
+                  ({tasks_completed}/{total_tasks})"
+            )
+            for title in titles_completed:
+                print(f"\t {title}")
 
 
 if __name__ == "__main__":
-    data_from_api()
+    get_employee()
