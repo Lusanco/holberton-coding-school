1d2f30f460281d156f11da626c66235bf5ddb202
lusanco <lasc1026@gmail.com>
2024-04-25 09:27:21 -0400

    testing -  w/3rd party

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python3
 
+# """
+# Returns the first element of a sequence
+# if it exists, otherwise returns None.
+
+# Args:
+#     lst (Sequence[Any]): The sequence
+#     to get the first element from.
+
+# Returns:
+#     Optional[Any]: The first element
+#     of the sequence or None if empty.
+# """
+
+# from typing import Sequence, Any, Optional
+
+
+# def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+#     """Returns the first element of a sequence or None."""
+#     # return lst[0] if lst else None
+#     if lst:
+#         return lst[0]
+#     else:
+#         return None
+
+# Trying third party code / unknown error
 """
-Returns the first element of a sequence
-if it exists, otherwise returns None.
-
-Args:
-    lst (Sequence[Any]): The sequence
-    to get the first element from.
-
-Returns:
-    Optional[Any]: The first element
-    of the sequence or None if empty.
+    Get first element of sequence.
 """
-
 from typing import Sequence, Any, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
-    """Returns the first element of a sequence or None."""
-    # return lst[0] if lst else None
+    """
+        get frist element of sequence or return none
+    """
     if lst:
         return lst[0]
     else:
