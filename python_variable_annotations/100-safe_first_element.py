adfed32242e7b1b13347f45cfc6ec0fef3576425
lusanco <lasc1026@gmail.com>
2024-04-25 09:36:07 -0400

    testing - trying doc the function

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -17,6 +17,11 @@ from typing import Sequence, Any, Optional
 
 # The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """
+    Return the first element of lst if lst is true,
+    else return none. 
+    """
+    
     if lst:
         return lst[0]
     else:
