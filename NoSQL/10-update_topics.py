24f2c8212936f37a526c4d8b40344404945e914b
lusanco <lasc1026@gmail.com>
2024-05-02 09:51:33 -0400

    code/add - update topics/gitignore

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+    Pytho script that updates a document in a collection based on name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Function that updates a document in a collection based on name
+    """
+    if mongo_collection is None:
+        return None
+    else:
+        mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+        return None
