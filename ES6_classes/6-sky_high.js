6f502ea31d971b958a1374fc914f5ace9c1a5266
lusanco <lasc1026@gmail.com>
2024-05-08 15:16:50 -0400

    eslint

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,16 +1,19 @@
-import Building from "./5-building";
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
+
   get floors() {
     return this._floors;
   }
+
   set floors(value) {
     this._floors = value;
   }
+
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
