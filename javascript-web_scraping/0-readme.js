2e6395b4102ed9f706021e50dbbae8b88b7572b7
lusanco <lasc1026@gmail.com>
2024-05-13 14:24:01 -0400

    testing checkers after

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
 const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error("Error: Please provide a file path as an argument.");
+  console.error('Error: Please provide a file path as an argument.');
   process.exit(1);
 }
 
-fs.readFile(filePath, "utf-8", (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     // Convert error object to a more informative JSON-like format
     console.error({
-      // message: 'Error reading file:',
       error: err,
     });
   } else {
