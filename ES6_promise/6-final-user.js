138444c2bac3ed052df964c9029f66d0780749ab
lusanco <lasc1026@gmail.com>
2024-05-15 15:35:02 -0400

    quick test

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,8 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(([user, uploadResult]) => [
-      { status: 'success', value: user },
-      { status: 'success', value: uploadResult },
-    ])
-    .catch((error) => [
-      { status: 'failed', value: error },
-      { status: 'failed', value: error },
-    ]);
+    .then((success) => [{ status: "success", value: success }])
+    .catch((error) => [{ status: "failed", value: error }]);
 }
