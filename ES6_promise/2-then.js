0e9e6544fb46fd957bf833081f17c5980e8106f7
lusanco <lasc1026@gmail.com>
2024-05-15 12:39:30 -0400

    testing then 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,15 @@
 export default function handleResponseFromAPI(promise) {
-  if (promise) {
-    promise.then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    });
-  } else {
-    promise.catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+  if (!promise) {
+    throw new Error('Invalid promise provided'); // Handle missing promise
   }
+
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' }; // Return success object
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error(); // Return empty error object
+    });
 }
