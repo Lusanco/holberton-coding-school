2735c5a34d666eb88b43e633c48cfa11247afafc
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-08-05 09:20:41 -0400

    initial setup

---
+++ markdown2html.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+A script that takes two arguments: input Markdown file and output HTML file.
+Usage: ./markdown2html.py <input_file> <output_file>
+"""
+
+import sys
+import os
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    input_file, output_file = sys.argv[1], sys.argv[2]
+
+    if not os.path.isfile(input_file):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(0)
